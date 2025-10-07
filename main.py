3f4fcd8c4f3c7da57d09e367ac3edf9d347a703e
remo <omerm42t@gmail.com>
2025-10-06 22:58:33 -0400

    hopefully added bookbot :)

---
+++ main.py
@@ -0,0 +1,31 @@
+from stats import split_function
+from stats import count_characters
+from stats import sort_on
+from stats import sorted_dictionary
+import sys
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    book = get_book_text(file_path)
+    word_count = split_function(book)
+    char_count = count_characters(book)
+    new_dict = sorted_dictionary(book)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in new_dict:
+   	 print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
+main()
+
+
---
+++ stats.py
@@ -0,0 +1,35 @@
+
+def split_function(book):
+    return len(book.split())
+
+def count_characters(book):
+    character_count = {}
+    lower_case = book.lower()
+    for char in lower_case:
+        if char not in character_count:
+            character_count[char] = 1
+        else:
+            character_count[char] += 1
+    return character_count
+
+
+def sort_on(item):
+    return item["num"]
+
+
+def sorted_dictionary(book):
+    sorted_dict = []
+    character_count = count_characters(book)
+    for ch in character_count:
+        cnt = character_count[ch]
+        if ch.isalpha():
+            sorted_dict.append({"char":ch,"num":cnt})
+    sorted_dict.sort(reverse=True, key=sort_on)
+
+    for item in sorted_dict:
+        print(item["char"], ":", item["num"])
+    
+    return sorted_dict
+
+
+
